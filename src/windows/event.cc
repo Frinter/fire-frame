d2536e0b658b9d9f40578a51f200b50b2de4fda9
Frinter <codewad@gmail.com>
2015-10-08 19:47:15 -0700

    fix some whitepsace

--- src/windows/event.cc
+++ src/windows/event.cc
@@ -5,32 +5,32 @@
 class WindowsEvent : public System::Event
 {
 public:
-	WindowsEvent(const char *name)
+    WindowsEvent(const char *name)
 	{
-		m_eventHandle = CreateEvent(NULL, true, false, name);
-		ResetEvent(m_eventHandle);
+            m_eventHandle = CreateEvent(NULL, true, false, name);
+            ResetEvent(m_eventHandle);
 	}
 		
-	virtual void Trigger()
+    virtual void Trigger()
 	{
-		SetEvent(m_eventHandle);
+            SetEvent(m_eventHandle);
 	}
 	
-	virtual void Wait()
+    virtual void Wait()
 	{
-		WaitForSingleObject(m_eventHandle, INFINITE);
+            WaitForSingleObject(m_eventHandle, INFINITE);
 	}
 	
-	virtual void Wait(int millisecondTimeout)
+    virtual void Wait(int millisecondTimeout)
 	{
-		WaitForSingleObject(m_eventHandle, millisecondTimeout);
+            WaitForSingleObject(m_eventHandle, millisecondTimeout);
 	}
 
-	private:
-	HANDLE m_eventHandle;
+private:
+    HANDLE m_eventHandle;
 };
 
 System::Event *System::Event::Create(const char *name)
 {
-	return new WindowsEvent(name);
+    return new WindowsEvent(name);
 }
