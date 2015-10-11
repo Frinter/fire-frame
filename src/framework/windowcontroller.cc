7148b2d6832c3704a9ee65e27619ba6c480b6103
Frinter <codewad@gmail.com>
2015-10-11 15:07:45 -0700

    Remove debug statement and fix indentation

--- src/framework/windowcontroller.cc
+++ src/framework/windowcontroller.cc
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "framework/keystate.hh"
 #include "framework/readingkeyboardstate.hh"
 #include "framework/windowcontroller.hh"
@@ -13,110 +11,109 @@ using Framework::ReadingKeyboardState;
 using Framework::WindowController;
 
 WindowController::WindowController(ApplicationContext *applicationContext)
-	: m_window(NULL), m_openGLContext(NULL), m_shouldDestroyWindow(false), m_applicationContext(applicationContext)
+    : m_window(NULL), m_openGLContext(NULL), m_shouldDestroyWindow(false), m_applicationContext(applicationContext)
 { }
 
 WindowController::~WindowController()
 {
-	if (m_openGLContext != NULL)
-		delete m_openGLContext;
-	if (m_window != NULL)
-		delete m_window;
+    if (m_openGLContext != NULL)
+        delete m_openGLContext;
+    if (m_window != NULL)
+        delete m_window;
 }
 
 void WindowController::CreateClientWindow()
 {
-	m_window = Window::Create(m_applicationContext, this);
-	m_window->DoMessageLoop();
+    m_window = Window::Create(m_applicationContext, this);
+    m_window->DoMessageLoop();
 }
 
 void WindowController::CreateContext()
 {
-	std::cout << "window controller CreateContext" << std::endl;
-	m_openGLContext = OpenGLContext::Create(m_window);
+    m_openGLContext = OpenGLContext::Create(m_window);
 }
 
 void WindowController::SwapBuffers()
 {
-	m_openGLContext->SwapBuffers();
+    m_openGLContext->SwapBuffers();
 }
 
 ReadingKeyboardState *WindowController::GetKeyStateReader()
 {
-	return &m_keyboardState;
+    return &m_keyboardState;
 }
 
 void WindowController::OnWindowReady()
 {
-	m_applicationContext->WindowReady()->Trigger();
+    m_applicationContext->WindowReady()->Trigger();
 }
 
 void WindowController::OnWindowClose()
 {
-	m_applicationContext->Close();
+    m_applicationContext->Close();
 }
 
 void WindowController::OnKeyDown(KeyCode key)
 {
-	m_keyboardState.PressKey(key);
+    m_keyboardState.PressKey(key);
 
-	for (std::list<IWritingKeyboardState*>::iterator iter = m_keyboardEventHandlers.begin(); iter != m_keyboardEventHandlers.end(); iter++)
-	{
-		(*iter)->PressKey(key);
-	}
+    for (std::list<IWritingKeyboardState*>::iterator iter = m_keyboardEventHandlers.begin(); iter != m_keyboardEventHandlers.end(); iter++)
+    {
+        (*iter)->PressKey(key);
+    }
 }
 
 void WindowController::OnKeyUp(KeyCode key)
 {
-	m_keyboardState.UnpressKey(key);
+    m_keyboardState.UnpressKey(key);
 
-	for (std::list<IWritingKeyboardState*>::iterator iter = m_keyboardEventHandlers.begin(); iter != m_keyboardEventHandlers.end(); iter++)
-	{
-		(*iter)->UnpressKey(key);
-	}
+    for (std::list<IWritingKeyboardState*>::iterator iter = m_keyboardEventHandlers.begin(); iter != m_keyboardEventHandlers.end(); iter++)
+    {
+        (*iter)->UnpressKey(key);
+    }
 }
 
 WindowController::KeyboardState::KeyboardState()
 {
-	m_mutex = Mutex::Create();
+    m_mutex = Mutex::Create();
 }
 
 WindowController::KeyboardState::~KeyboardState()
 {
-	delete m_mutex;
+    delete m_mutex;
 }
 
 KeyState WindowController::KeyboardState::GetKeyState(KeyCode key)
 {
-	KeyState state;
+    KeyState state;
 
-	m_mutex->Lock();
-	state = m_states[key];
-	m_mutex->Unlock();
+    m_mutex->Lock();
+    state = m_states[key];
+    m_mutex->Unlock();
 
-	return state;
+    return state;
 }
 
 void WindowController::KeyboardState::PressKey(KeyCode key)
 {
-	m_mutex->Lock();
-	m_states[key] = Pressed;
-	m_mutex->Unlock();
+    m_mutex->Lock();
+    m_states[key] = Pressed;
+    m_mutex->Unlock();
 }
 
 void WindowController::KeyboardState::UnpressKey(KeyCode key)
 {
-	m_mutex->Lock();
-	m_states[key] = Unpressed;
-	m_mutex->Unlock();
+    m_mutex->Lock();
+    m_states[key] = Unpressed;
+    m_mutex->Unlock();
 }
 
 void WindowController::AddKeyboardEventHandler(IWritingKeyboardState *handler)
 {
-	m_keyboardEventHandlers.push_back(handler);
+    m_keyboardEventHandlers.push_back(handler);
 }
 
 void WindowController::RemoveKeyboardEventHandler(IWritingKeyboardState *handler)
 {
-	m_keyboardEventHandlers.remove(handler);
+    m_keyboardEventHandlers.remove(handler);
 }
