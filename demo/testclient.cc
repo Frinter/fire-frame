67d63945ebfcaa470a47add26a9cf54066d10db5
Frinter <codewad@gmail.com>
2016-06-18 20:40:49 -0700

    Fix some build things

--- demo/testclient.cc
+++ demo/testclient.cc
@@ -18,14 +18,14 @@ class SleepService : public ISleepService
 public:
     SleepService(System::Utility *utility)
         : m_utility(utility)
-	{
-	}
-	
+    {
+    }
+
     virtual void Sleep(unsigned int milliseconds)
-	{
-            m_utility->Sleep(milliseconds);
-	}
-	
+    {
+        m_utility->Sleep(milliseconds);
+    }
+
 private:
     System::Utility *m_utility;
 };
@@ -35,95 +35,95 @@ class SystemTimer : public ISystemTimer
 public:
     SystemTimer(System::Utility *utility)
         : m_utility(utility)
-	{
-	}
+    {
+    }
 
     virtual unsigned int GetTicks()
-	{
-            return m_utility->GetTicks();
-	}
+    {
+        return m_utility->GetTicks();
+    }
 
 private:
     System::Utility *m_utility;
 };
 
 class OtherTestController : public GameController {
-public:	
+public:
     virtual void OnStackAdd()
-	{
-            std::cout << "OtherTestController added to stack" << std::endl;
-	}
+    {
+        std::cout << "OtherTestController added to stack" << std::endl;
+    }
 
     virtual void OnStackRemove()
-	{
-            std::cout << "OtherTestController removed from stack" << std::endl;
-	}	
+    {
+        std::cout << "OtherTestController removed from stack" << std::endl;
+    }
 
     virtual void OnStackFocus()
-	{
-            std::cout << "OtherTestController stack focus" << std::endl;
-	}
+    {
+        std::cout << "OtherTestController stack focus" << std::endl;
+    }
 
     virtual void OnStackBlur()
-	{
-            std::cout << "OtherTestController stack blur" << std::endl;
-	}
+    {
+        std::cout << "OtherTestController stack blur" << std::endl;
+    }
 };
 
 class TestController : public GameController {
 public:
     TestController()
         : m_otherController(NULL)
-	{
-	}
-	
+    {
+    }
+
     ~TestController()
-	{
-            if (m_otherController != NULL)
-            {
-                GetControllerStack()->Pop();
-                delete m_otherController;
-                m_otherController = NULL;			
-            }
-	}
-	
+    {
+        if (m_otherController != NULL)
+        {
+            GetControllerStack()->Pop();
+            delete m_otherController;
+            m_otherController = NULL;
+        }
+    }
+
     virtual void OnStackAdd()
-	{
-            std::cout << "TestController added to stack" << std::endl;
-	}
+    {
+        std::cout << "TestController added to stack" << std::endl;
+    }
 
     virtual void OnStackRemove()
-	{
-            std::cout << "TestController removed from stack" << std::endl;
-	}
-	
+    {
+        std::cout << "TestController removed from stack" << std::endl;
+    }
+
     virtual void OnStackFocus()
-	{
-            std::cout << "TestController stack focus" << std::endl;
-	}
+    {
+        std::cout << "TestController stack focus" << std::endl;
+    }
 
     virtual void OnStackBlur()
-	{
-            std::cout << "TestController stack blur" << std::endl;
-	}
+    {
+        std::cout << "TestController stack blur" << std::endl;
+    }
 
     virtual void OnTick()
-	{
-            ReadingKeyboardState *keyboardState = GetKeyboardState();
+    {
+        ReadingKeyboardState *keyboardState = GetKeyboardState();
 
-            if (m_otherController == NULL)
-            {
-                m_otherController = new OtherTestController();
-                GetControllerStack()->Push(m_otherController);
-            }
-            else
-            {
-                GetControllerStack()->Pop();
-                delete m_otherController;
-                m_otherController = NULL;
-            }
-	}
-	
+        if (m_otherController == NULL)
+        {
+            m_otherController = new OtherTestController();
+            GetControllerStack()->Push(m_otherController);
+        }
+        else
+        {
+            GetControllerStack()->Pop();
+            delete m_otherController;
+            m_otherController = NULL;
+        }
+    }
+
 private:
     OtherTestController *m_otherController;
 };
@@ -137,7 +137,7 @@ GetApplicationState_FunctionSignature(GetApplicationState)
     return &applicationState;
 }
 
-GraphicsThreadEntry_FunctionSignature(GraphicsThreadEntry)
+ApplicationThreadEntry_FunctionSignature(ApplicationThreadEntry)
 {
     std::cout << "GraphicsThreadEntry: " << std::endl;
 
@@ -147,30 +147,14 @@ GraphicsThreadEntry_FunctionSignature(GraphicsThreadEntry)
     const GLubyte *vendor = glGetString(GL_VENDOR);
     const GLubyte *version = glGetString(GL_VERSION);
     const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
-	
+
     std::cout << "- renderer: " << renderer << std::endl;
     std::cout << "- vendor: " << vendor << std::endl;
     std::cout << "- version: " << version << std::endl;
     std::cout << "- GLSL version: " << version << std::endl;
 
-    //GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    //GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
     unsigned int framerateTicks = 1000 / 60;
 
-    SystemTimer systemTimer(applicationContext->GetSystemUtility());
-    SleepService sleepService(applicationContext->GetSystemUtility());
-    Ticker ticker = Ticker(&systemTimer, &sleepService);
-    ticker.Start();
-
-    while (!applicationContext->IsClosing())
-    {
-        ticker.Wait(framerateTicks);
-    }
-}
-
-LogicThreadEntry_FunctionSignature(LogicThreadEntry)
-{
     SystemTimer systemTimer(applicationContext->GetSystemUtility());
     SleepService sleepService(applicationContext->GetSystemUtility());
     Ticker ticker = Ticker(&systemTimer, &sleepService);
@@ -189,12 +173,12 @@ LogicThreadEntry_FunctionSignature(LogicThreadEntry)
     {
         controller = (GameController *)controllerStack.Top();
         controller->OnTick();
-        
+
         newTicks = systemTimer.GetTicks();
         ticker.Wait(500);
-        std::cout << "Logic Thread (" << ticks << " -> " << newTicks << ")" << std::endl;
-        ticks = newTicks;			
+        std::cout << "Application Thread (" << ticks << " -> " << newTicks << ")" << std::endl;
+        ticks = newTicks;
     }
-    
+
     controllerStack.Clear();
 }
--- makefile
+++ makefile
@@ -6,15 +6,15 @@ ifeq ($(SYSTEM_TARGET),MINGW)
   PLATFORM_LIBS = -lmingw32
   PLATFORM_POST_LIBS = -lgdi32 -lopengl32 -lwinmm
   PLATFORM_LINKFLAGS = -Wl,-subsystem,windows
-  PLATFORM_MAIN_OBJECTS = 
+  PLATFORM_MAIN_OBJECTS =
   PLATFORM_OBJECTS = $(patsubst src/%.c,build/%.o,$(wildcard src/windows/*.c)) $(patsubst src/%.cc,build/%.o,$(wildcard src/windows/*.cc)) $(patsubst src/%.cc,build/%.o,$(wildcard src/mingw/*.cc))
 
 include $(wildcard build/windows/*.d)
 else
   CXX = g++
   PLATFORM_LIBS = -lGL
-  PLATFORM_POST_LIBS = 
-  PLATFORM_LINKFLAGS = 
+  PLATFORM_POST_LIBS =
+  PLATFORM_LINKFLAGS =
   PLATFORM_OBJECTS =
 
 include $(wildcard build/linux/*.d)
@@ -38,7 +38,7 @@ TEST_OBJECTS := $(patsubst test/%.cc,build/%.o,$(TEST_SRC))
 TESTABLES_OBJECTS := $(OBJ_DIR)/demo/ticker.o
 TEST_INCLUDE := -Idemo
 TEST_CFLAGS := $(TEST_INCLUDE)
-TEST_LIBS := 
+TEST_LIBS :=
 TEST_TARGET := $(BIN_DIR)/test
 TEST_LINK_FLAGS := -static-libgcc -static-libstdc++
 
@@ -60,36 +60,28 @@ $(BUILD_DIRS) $(OBJ_DIR) $(BIN_DIR):
 	mkdir -p $@
 
 $(OBJ_DIR)/%.o: src/%.c $(OBJ_DIR)/%.d
-	@echo Building $@
-	@clang -c -o $@ $(CFLAGS) $<
+	gcc -c -o $@ $(CFLAGS) $<
 
 $(OBJ_DIR)/%.o: src/%.cc $(OBJ_DIR)/%.d
-	@echo Building $@
-	@clang++ -c -o $@ $(CFLAGS) -std=c++11 $<
+	g++ -c -o $@ $(CFLAGS) -std=c++11 $<
 
 $(OBJ_DIR)/%.o: test/%.cc $(OBJ_DIR)/%.d
-	@echo '(Test) Building $@'
-	@clang++ -c -o $@ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) $<
+	g++ -c -o $@ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) $<
 
 $(OBJ_DIR)/demo/%.o: demo/%.cc $(OBJ_DIR)/demo/%.d
-	@echo '(Demo) Building $@'
-	@clang++ -c -o $@ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) $<
+	g++ -c -o $@ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) $<
 
 $(OBJ_DIR)/%.d: src/%.c
-#	@echo 'Building dependencies for $< -> $@'
-	@clang $(CFLAGS) -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
+	gcc $(CFLAGS) -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
 
 $(OBJ_DIR)/%.d: src/%.cc
-#	@echo 'Building dependencies for $< -> $@'
-	@clang++ $(CFLAGS) -std=c++11 -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
+	g++ $(CFLAGS) -std=c++11 -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
 
 $(OBJ_DIR)/%.d: test/%.cc
-#	@echo '(Test) Building dependencies for $< -> $@'
-	@clang++ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
+	g++ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
 
 $(OBJ_DIR)/demo/%.d: demo/%.cc
-#	@echo '(Demo) Building dependencies for $< -> $@'
-	@clang++ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
+	g++ $(CFLAGS) -std=c++11 $(TEST_CFLAGS) -MM -MT $(OBJ_DIR)/$*.o -MF $@ $<
 
 release: $(PLATFORM_LAYER_TARGET)
 demo: $(DEMO_TARGET)
@@ -97,15 +89,12 @@ test: $(TEST_TARGET)
 	@$(TEST_TARGET)
 
 $(PLATFORM_LAYER_TARGET): $(OBJECTS)
-	@echo Linking: $@
 	ar cr $@ $^
 
 $(DEMO_TARGET): $(DEMO_OBJECTS) $(PLATFORM_LAYER_TARGET)
-	@echo '(Demo) Linking: $@'
-	clang++ $(PLATFORM_LINK_FLAGS) -static-libgcc -static-libstdc++ -Iinclude -o $@ $(DEMO_OBJECTS) $(PLATFORM_LIBS) $(PLATFORM_LAYER_TARGET) $(PLATFORM_POST_LIBS)
+	g++ $(PLATFORM_LINK_FLAGS) -static-libgcc -static-libstdc++ -Iinclude -o $@ $(DEMO_OBJECTS) $(PLATFORM_LIBS) $(PLATFORM_LAYER_TARGET) $(PLATFORM_POST_LIBS)
 
 $(TEST_TARGET): $(TEST_OBJECTS) $(TESTABLES_OBJECTS)
-	@echo '(Test) Linking: $@'
-	@clang++ -g $(TEST_LINK_FLAGS) -o $@ $^
+	g++ -g $(TEST_LINK_FLAGS) -o $@ $^
 
 include $(wildcard build/*.d)
