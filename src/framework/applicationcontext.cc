7fbba1af5262035fe42f8324cc81e0c517bb9c80
Frinter <codewad@gmail.com>
2016-06-21 23:05:14 -0700

    Improve some whitespace

--- src/framework/applicationcontext.cc
+++ src/framework/applicationcontext.cc
@@ -3,48 +3,48 @@
 using namespace Framework;
 
 ApplicationContext::ApplicationContext(ApplicationState *applicationState)
-	: m_isClosing(false), m_destroyWindowFlag(false),
-          m_applicationState(applicationState), m_windowReady(System::Event::Create("WindowReady")),
-          m_applicationThreadQuit(System::Event::Create("GraphicsThreadQuit"))
+    : m_isClosing(false), m_destroyWindowFlag(false),
+      m_applicationState(applicationState), m_windowReady(System::Event::Create("WindowReady")),
+      m_applicationThreadQuit(System::Event::Create("GraphicsThreadQuit"))
 {
 }
 
 void ApplicationContext::Close()
 {
-	m_isClosing = true;
+    m_isClosing = true;
 }
 
 void ApplicationContext::SignalWindowDestruction()
 {
-	m_destroyWindowFlag = true;
+    m_destroyWindowFlag = true;
 }
 
 bool ApplicationContext::IsClosing() const
 {
-	return m_isClosing;
+    return m_isClosing;
 }
 
 bool ApplicationContext::ShouldDestroyWindow() const
 {
-	return m_isClosing || m_destroyWindowFlag;
+    return m_isClosing || m_destroyWindowFlag;
 }
 
 System::Event *ApplicationContext::WindowReady() const
 {
-	return m_windowReady;
+    return m_windowReady;
 }
 
 System::Event *ApplicationContext::ApplicationThreadQuit() const
 {
-	return m_applicationThreadQuit;
+    return m_applicationThreadQuit;
 }
 
 ApplicationState *ApplicationContext::GetState() const
 {
-	return m_applicationState;
+    return m_applicationState;
 }
 
 System::Utility *ApplicationContext::GetSystemUtility() const
 {
-	return System::Utility::GetInstance();
+    return System::Utility::GetInstance();
 }
