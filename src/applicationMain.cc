3036bb35dc7cef730908f928e169e3922386b783
Frinter <codewad@gmail.com>
2017-10-09 22:10:18 -0700

    Remove unneeded references

--- src/applicationMain.cc
+++ src/applicationMain.cc
@@ -1,12 +1,10 @@
 #include "framework/applicationcontext.hh"
-#include "framework/windowcontroller.hh"
 #include "framework/clientcode.hh"
 #include "system/thread.hh"
 
 using Framework::ApplicationContext;
 using Framework::ClientCode;
 using Framework::LoadClientCode;
-using Framework::WindowController;
 
 using System::thread;
 
