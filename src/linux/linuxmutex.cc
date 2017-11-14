7745d714067befb9923c3cdd96733787dd0ab824
Frinter <codewad@gmail.com>
2017-11-14 08:56:17 -0800

    Implement mutex for linux

--- src/linux/linuxmutex.cc
+++ src/linux/linuxmutex.cc
@@ -1,14 +1,34 @@
-#include "linuxmutex.hh"
+#include <pthread.h>
+#include "system/mutex.hh"
+
+class LinuxMutex : public System::Mutex
+{
+public:
+    LinuxMutex()
+    {
+        pthread_mutex_init(&_handle, NULL);
+    }
+
+    ~LinuxMutex()
+    {
+        pthread_mutex_destroy(&_handle);
+    }
+
+    void Lock()
+    {
+        pthread_mutex_lock(&_handle);
+    }
+
+    void Unlock()
+    {
+        pthread_mutex_unlock(&_handle);
+    }
+
+private:
+    pthread_mutex_t _handle;
+};
 
 System::Mutex *System::Mutex::Create()
 {
     return new LinuxMutex();
-}
-
-void LinuxMutex::Lock()
-{
-}
-
-void LinuxMutex::Unlock()
-{
 }
