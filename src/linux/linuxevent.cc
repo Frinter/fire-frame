9bcc0da17744fffc2181505aa2883fdd6901e269
Frinter <codewad@gmail.com>
2017-11-19 17:46:10 -0800

    Implement threading events for linux

--- src/linux/linuxevent.cc
+++ src/linux/linuxevent.cc
@@ -1,9 +1,55 @@
-#include "linuxevent.hh"
+#include <poll.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+#include "system/event.hh"
+
+class LinuxEvent : public System::Event
+{
+public:
+    LinuxEvent()
+    {
+        _handle = eventfd(0, 0);
+    }
+
+    ~LinuxEvent()
+    {
+        close(_handle);
+    }
+
+    void Trigger()
+    {
+        eventfd_write(_handle, 1);
+    }
+
+    void Wait()
+    {
+        Wait(-1);
+    }
+
+    void Wait(int millisecondTimeout)
+    {
+        struct pollfd info;
+        info.fd = _handle;
+        info.events = POLLIN;
+        info.revents = 0;
+
+        poll(&info, 1, millisecondTimeout);
+
+        eventfd_t buffer;
+        eventfd_read(_handle, &buffer);
+    }
+
+private:
+    int _handle;
+};
 
 System::Event *System::Event::Create(const char *name)
 {
+    return new LinuxEvent();
 }
 
 System::Event *System::Event::Create()
 {
+    return new LinuxEvent();
 }
