1a4b05d845e465a98ebd725b85da3b76ceaf1598
Frinter <codewad@gmail.com>
2017-12-02 18:25:17 -0800

    Switch test framework to catch2

--- test/testmain.cc
+++ test/testmain.cc
@@ -1,26 +1,3 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN
 
-#include "tests.hh"
-
-std::vector<Tests::TestRunner*> *Tests::Registry::_runners = NULL;
-
-int main()
-{
-    std::vector<Tests::TestRunner*> *runners = Tests::Registry::GetInstance();
-    std::cout << "Running " << runners->size() << " tests" << std::endl;
-    for (auto iterator = runners->begin(); iterator != runners->end(); iterator++)
-    {
-        try
-        {
-            (*iterator)->Run();
-        }
-        catch (...)
-        {
-            std::cout << "Test failed" << std::endl;
-            return 1;
-        }
-    }
-
-    std::cout << "All tests passed" << std::endl;
-    return 0;
-}
+#include "catch.hpp"
--- test/testticker.cc
+++ test/testticker.cc
@@ -1,94 +1,95 @@
+#include "catch.hpp"
+
 #include "ticker.hh"
-#include "tests.hh"
 
-FIXTURE("Ticker")
+TEST_CASE("Ticker")
 {
-	class MockTimer : public ISystemTimer
-	{
-	public:
-		MockTimer()
-			: wasCalled(false)
-		{
-			ticks = 1;
-		}
+    class MockTimer : public ISystemTimer
+    {
+    public:
+        MockTimer()
+            : wasCalled(false)
+        {
+            ticks = 1;
+        }
 
-		virtual unsigned int GetTicks()
-		{
-			wasCalled = true;
-			return ticks;
-		}
+        virtual unsigned int GetTicks()
+        {
+            wasCalled = true;
+            return ticks;
+        }
 
-		int ticks;
-		bool wasCalled;
-	};
+        int ticks;
+        bool wasCalled;
+    };
 
-	class MockSleeper : public ISleepService
-	{
-	public:
-		MockSleeper()
-			: sleepArg(0), wasCalled(false)
-		{
-		}
+    class MockSleeper : public ISleepService
+    {
+    public:
+        MockSleeper()
+            : sleepArg(0), wasCalled(false)
+        {
+        }
 
-		virtual void Sleep(unsigned int milliseconds)
-		{
-			wasCalled = true;
-			sleepArg = milliseconds;
-		}
+        virtual void Sleep(unsigned int milliseconds)
+        {
+            wasCalled = true;
+            sleepArg = milliseconds;
+        }
 
-		unsigned int sleepArg;
-		bool wasCalled;
-	};
+        unsigned int sleepArg;
+        bool wasCalled;
+    };
 
-	TEST("Ticker logs ticks on start")
-	{
-		MockTimer timer = MockTimer();
-		MockSleeper sleeper = MockSleeper();
-		Ticker ticker = Ticker(&timer, &sleeper);
+    SECTION("Ticker logs ticks on start")
+    {
+        MockTimer timer = MockTimer();
+        MockSleeper sleeper = MockSleeper();
+        Ticker ticker = Ticker(&timer, &sleeper);
 
-		assert(timer.wasCalled == false);
-		ticker.Start();
-		assert(timer.wasCalled == true);
-	}
+        REQUIRE(timer.wasCalled == false);
+        ticker.Start();
+        REQUIRE(timer.wasCalled == true);
+    }
 
-	TEST("Ticker sleeps for proper time")
-	{
-		MockTimer timer = MockTimer();
-		MockSleeper sleeper = MockSleeper();
-		Ticker ticker = Ticker(&timer, &sleeper);
-		
-		timer.ticks = 1;
-		ticker.Start();
-		timer.ticks = 10;
-		ticker.Wait(10);
-		assert(sleeper.sleepArg == 1);
-	}
-	
-	TEST("Ticker does not sleep when waiting for less than elapsed time")
-	{
-		MockTimer timer = MockTimer();
-		MockSleeper sleeper = MockSleeper();
-		Ticker ticker = Ticker(&timer, &sleeper);
+    SECTION("Ticker sleeps for proper time")
+    {
+        MockTimer timer = MockTimer();
+        MockSleeper sleeper = MockSleeper();
+        Ticker ticker = Ticker(&timer, &sleeper);
 
-		timer.ticks = 1;
-		ticker.Start();
-		timer.ticks = 20;
-		ticker.Wait(10);
-		assert(sleeper.wasCalled == false);
-	}
-	
-	TEST("Subsequent wait calls sleep for appropriate amounts of time")
-	{
-		MockTimer timer = MockTimer();
-		MockSleeper sleeper = MockSleeper();
-		Ticker ticker = Ticker(&timer, &sleeper);
+        timer.ticks = 1;
+        ticker.Start();
+        timer.ticks = 10;
+        ticker.Wait(10);
+        REQUIRE(sleeper.sleepArg == 1);
+    }
 
-		timer.ticks = 1;
-		ticker.Start();
-		timer.ticks = 20;
-		ticker.Wait(10);
-		ticker.Wait(10);
-		assert(sleeper.wasCalled == true);
-		assert(sleeper.sleepArg == 1);
-	}
+    SECTION("Ticker does not sleep when waiting for less than elapsed time")
+    {
+        MockTimer timer = MockTimer();
+        MockSleeper sleeper = MockSleeper();
+        Ticker ticker = Ticker(&timer, &sleeper);
+
+        timer.ticks = 1;
+        ticker.Start();
+        timer.ticks = 20;
+        ticker.Wait(10);
+        REQUIRE(sleeper.wasCalled == false);
+    }
+
+    SECTION("Subsequent wait calls sleep for appropriate amounts of time")
+    {
+        MockTimer timer = MockTimer();
+        MockSleeper sleeper = MockSleeper();
+        Ticker ticker = Ticker(&timer, &sleeper);
+
+        timer.ticks = 1;
+        ticker.Start();
+        timer.ticks = 20;
+        ticker.Wait(10);
+        ticker.Wait(10);
+        REQUIRE(sleeper.wasCalled == true);
+        REQUIRE(sleeper.sleepArg == 1);
+    }
 }
